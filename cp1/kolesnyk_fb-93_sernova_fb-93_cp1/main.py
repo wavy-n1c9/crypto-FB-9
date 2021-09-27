4be205ec1a730f6f8b686dbb998162af8ba320fa
Andrii Kolesnyk <83187993+wavy-n1c9@users.noreply.github.com>
2021-09-27 23:21:25 +0300

    final code(navernoe)

--- cp1/kolesnyk_fb-93_sernova_fb-93_cp1/main.py
+++ cp1/kolesnyk_fb-93_sernova_fb-93_cp1/main.py
@@ -1,24 +1,18 @@
 import re
 import math
 
-
-def filter_str(str_txt):
-    str_txt = re.sub("[^А-Яа-я]", "", str_txt)
-    str_txt = str_txt.lower()
-    return str_txt
-
-
-def entrop_let(str_txt, filename):
+def entrop_let(str_txt, filename, letters):
+    lettersq=len(letters)
     entr=0
-    letters = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
-    let_q = [None] * 32
-    let_freq_mass = [None] * 32
+    let_q = [None] * lettersq
+    let_freq_mass = [None] * lettersq
     len_txt = len(str_txt)
-    for i in range(0, 32):
+    for i in range(0, lettersq):
         let_freq_mass[i] = (str_txt.count(letters[i])) / len_txt
     # entrop:
-    for i in range(0, 32):
-        entr = entr + let_freq_mass[i] * math.log2(let_freq_mass[i])
+    for i in range(0, lettersq):
+        if(let_freq_mass[i]!=0):
+            entr = entr + let_freq_mass[i] * math.log2(let_freq_mass[i])
     entr=-entr
 
     freq_dict = dict(zip(letters, let_freq_mass))
@@ -30,22 +24,72 @@ def entrop_let(str_txt, filename):
                 sorted_dict[k] = freq_dict[k]
                 break
 
-    filename.write("\n" + "Sorted dict(letter+freq):\n" + str(sorted_dict))
-    filename.write("\nEntrop(for symb): " + str(entr))
+    filename.write("\n" + "Sorted dict(letter+freq):\n")
+    for let in letters:
+        filename.write(str(let)+" is "+str(sorted_dict[let])+"\n")
+    filename.write("\nEntrop: " + str(entr))
 
 
-def entrop():
-    bigrams=[]
+def entrop_bigr1(lett, textbigr,filename):
+    lentxt=len(textbigr)
+    alph_l=len(lett)
+    mass_bigr = [[lett[k] + lett[i] for i in range(alph_l)] for k in range(alph_l)]
+    values=[[None]*lentxt]*lentxt
+    for i in range(alph_l):
+        for j in range(alph_l):
+            freq=(textbigr.count(mass_bigr[i][j]) / lentxt)
+            values[i][j] = freq
+            mass_bigr[i][j]= mass_bigr[i][j]+(" is ")+str(freq)
+    for i in range(alph_l):
+        for j in range(alph_l):
+            filename.write(mass_bigr[i][j]+" ")
+        filename.write("\n")
+    #Пошук ентропії
+    entr=0
+    for i in range(alph_l):
+        for j in range(alph_l):
+            if (values[i][j] != 0):
+                entr = entr + (-(values[i][j]) * (math.log2(values[i][j])))
+    filename.write("Entropy is: " + str(entr))
 
 
-file1 = open('before.txt', 'r', encoding='utf-8')
-text_str = file1.read()
-text_str = filter_str(text_str)
 
-file2 = open('after.txt', 'w', encoding='utf-8')
-file2.write(text_str)
-entrop_let(text_str, file2)
+alpha = "абвгдежзийклмнопрстуфхцчшщыьэюя"
+alpha2 = "абвгдежзийклмнопрстуфхцчшщыьэюя "
+filesp = open('spaces.txt', 'r', encoding='utf-8') #filesp - file with spaces
+filensp = open('nospaces.txt', 'r', encoding='utf-8') #without spaces
+file2 = open('after.txt', 'w', encoding='utf-8') #result
 
+text_str_nsp = filensp.read()
+file2.write("Letters, no spaces: ")
+file2.write("entrop for letters, no spaces")
+entrop_let(text_str_nsp, file2, alpha)
+
+# Розбиття тексту на біграми
+
+txtbigramcross = [text_str_nsp[i:i+2] for i in range(len(text_str_nsp))] #перехресна
+txtbigramnocross = [text_str_nsp[i:i+2] for i in range(0, len(text_str_nsp), 2)] # не перехресна
+file2.write("\nBigram cross, no spaces \n")
+entrop_bigr1(alpha, txtbigramcross, file2)
+file2.write("\nBigram for no cross, no spaces \n")
+entrop_bigr1(alpha, txtbigramnocross, file2)
+
+
+#Робимо теж саме, але з пробілами
+
+text_sp = filesp.read()
+
+file2.write("\nEntrop for letters, with spaces\n")
+entrop_let(text_sp, file2, alpha2)
+
+txtbigramcross = [text_sp[i:i+2] for i in range(len(text_sp))] #перехресна
+txtbigramnocross = [text_sp[i:i+2] for i in range(0, len(text_sp), 2)] # не перехресна
+
+file2.write("\nBigram cross, with spaces \n")
+entrop_bigr1(alpha2, txtbigramcross, file2)
+file2.write("\nBigram for no cross, with spaces \n")
+entrop_bigr1(alpha2, txtbigramnocross, file2)
 
 file2.close()
-file1.close()
+filesp.close()
+filensp.close()
